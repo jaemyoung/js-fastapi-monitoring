7311296632be0614a043e537766388ee0d24156e
blue <jaemyoung95@gmail.com>
2024-05-27 09:43:34 +0000

    added prom instru

--- app/api.py
+++ app/api.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 import numpy as np
 from fastapi import FastAPI, Response
+import joblib
 from joblib import load
 from .schemas import Wine, Rating, feature_names
-# from .monitoring import instrumentator
+from .monitoring import instrumentator
 
 ROOT_DIR = Path(__file__).parent.parent
-
+print(ROOT_DIR)
 app = FastAPI()
 scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
 model = load(ROOT_DIR / "artifacts/model.joblib")
 
+instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
 
 @app.get("/")
 def root():
